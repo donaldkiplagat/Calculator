1b9000ba4ccbb0cd5bd58bce67a085172eb83e3c
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-07 10:06:36 +0300

    BMI Added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 =parseInt( prompt("Enter another number:"));
-alert(add(number1, number2));
+
+
+//BMI Calculation
+var  bodyMass=function(weight,height){
+  return weight/(height*height);
+}
+var weight=parseFloat(prompt("Enter your weight"));
+var height=parseFloat(prompt("Enter your height"));
+var result=bodyMass(weight,height);
+alert("Your BMI is "+result);
